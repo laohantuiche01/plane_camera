9ba57b82515feb80b2824690733dc51bc76e915a
laohantuiche01 <3056302107@qq.com>
2025-08-14 23:49:43 +0800

    识别效果不错（分辨率320）

--- src/camera_data_handle/include/Yolov8Detector/Yolov8Detector.h
+++ src/camera_data_handle/include/Yolov8Detector/Yolov8Detector.h
@@ -30,7 +30,7 @@ namespace Yolov8 {
 
         explicit YOLOv8Detector(const std::string &modelPath,
                                 const std::vector<std::string> &classNames = defaultClassNames,
-                                int imgSize = 640,
+                                int imgSize = 320,
                                 float confThreshold = 0.5,
                                 float nmsThreshold = 0.4);
 

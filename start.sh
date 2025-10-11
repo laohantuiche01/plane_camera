ddaafca351a4a626c3b2fae7b59ca6fcf0b93fd8
laohantuiche01 <3056302107@qq.com>
2025-10-11 19:40:30 +0800

    更新位置发布信息

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 gnome-terminal --tab -- bash -c "source install/setup.bash;ros2 launch realsense2_camera rs_launch.py rgb_camera.color_profile:=640x480x30"
 
-gnome-terminal --tab -- bash -c "source install/setup.bash;ros2 run camera_data_handle receive_data  "
+gnome-terminal --tab -- bash -c "source install/setup.bash;ros2 launch camera_data_handle camera.launch.py  "
 
 gnome-terminal --tab -- bash -c "source install/setup.bash;ros2 run camera_data_handle detect_publish  "
 
